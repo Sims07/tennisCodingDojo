92cf5066704da902b2a339f9fa64f862b7afbde7
Simon Chareyron <simonchareyron@MacBook-Pro-de-Simon.local>
2019-12-15 23:16:44 -0500

    Use static import in tests

--- src/test/java/simon/chareyron/coding/tennisrules/TennisRulesTest.java
+++ src/test/java/simon/chareyron/coding/tennisrules/TennisRulesTest.java
@@ -2,18 +2,18 @@ package simon.chareyron.coding.tennisrules;
 
 import org.junit.Test;
 
+import static simon.chareyron.coding.tennisrules.BDDTennisAssertions.*;
+
 public class TennisRulesTest {
 
     @Test
     public void winAPointFrom0() {
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-0")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-0")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("15-0");
 
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-0")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-0")
                 .whenPlayerWinPoint("2")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("0-15");
@@ -21,14 +21,12 @@ public class TennisRulesTest {
 
     @Test
     public void winAPointFrom15() {
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "15-0")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "15-0")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("30-0");
 
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-15")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-15")
                 .whenPlayerWinPoint("2")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("0-30");
@@ -36,14 +34,12 @@ public class TennisRulesTest {
 
     @Test
     public void winAPointFrom30() {
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "30-0")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "30-0")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("40-0");
 
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-30")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-30")
                 .whenPlayerWinPoint("2")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("0-40");
@@ -51,14 +47,12 @@ public class TennisRulesTest {
 
     @Test
     public void winAPointFrom4040() {
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-40")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-40")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("Av-40");
 
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-40")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-40")
                 .whenPlayerWinPoint("2")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("40-Av");
@@ -66,14 +60,12 @@ public class TennisRulesTest {
 
     @Test
     public void winAPointFrom40Av_Equality() {
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-Av")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-Av")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("40-40");
 
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "Av-40")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "Av-40")
                 .whenPlayerWinPoint("2")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("40-40");
@@ -81,14 +73,12 @@ public class TennisRulesTest {
 
     @Test
     public void winAPointAndAGame() {
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-0")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-0")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("1-0")
                 .thenGameScoreIs("0-0");
 
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-40")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-40")
                 .whenPlayerWinPoint("2")
                 .thenSetScoreIs("0-1")
                 .thenGameScoreIs("0-0");
@@ -96,14 +86,12 @@ public class TennisRulesTest {
 
     @Test
     public void winAPointAndAGameInAdvantage() {
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "Av-40")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "Av-40")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("1-0")
                 .thenGameScoreIs("0-0");
 
-        BDDTennisAssertions
-                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-Av")
+        givenTennisRuleAndInitScore(new TennisRules(), "0-0", "40-Av")
                 .whenPlayerWinPoint("2")
                 .thenSetScoreIs("0-1")
                 .thenGameScoreIs("0-0");
