5b09140482e216fe375c41292bd6504b00b1e5d5
Simon Chareyron <simonchareyron@MacBook-Pro-de-Simon.local>
2019-12-15 21:47:03 -0500

    TDD rules 0-15

--- src/test/java/simon/chareyron/coding/tennisrules/TennisRulesTest.java
+++ src/test/java/simon/chareyron/coding/tennisrules/TennisRulesTest.java
@@ -5,12 +5,18 @@ import org.junit.Test;
 public class TennisRulesTest {
 
     @Test
-    public void winAPoint() {
+    public void winAPointFrom0() {
         BDDTennisAssertions
                 .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-0")
                 .whenPlayerWinPoint("1")
                 .thenSetScoreIs("0-0")
                 .thenGameScoreIs("15-0");
+
+        BDDTennisAssertions
+                .givenTennisRuleAndInitScore(new TennisRules(), "0-0", "0-0")
+                .whenPlayerWinPoint("2")
+                .thenSetScoreIs("0-0")
+                .thenGameScoreIs("0-15");
     }
 
 
